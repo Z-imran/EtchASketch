65ff814430c6e224079b2c2f5e9c6f7a87f84cc9
Z-imran <148902089+Z-imran@users.noreply.github.com>
2025-07-29 02:54:35 -0600

    Added Buttons that control size, eraser, color.

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 const container = document.querySelector(".container");
+const eraserB = document.querySelector(".erase");
+const sizerB = document.querySelector(".sizer");
+const colorB = document.querySelector(".color");
+
+let mode = "color";
+let mouseDown = false
+document.body.onmousedown = () => {
+    mouseDown = true;
+};
+
+document.body.onmouseup = () => {
+    mouseDown = false;
+};
+
+eraserB.onclick = () => {
+    mode = "eraser"
+};
+
+colorB.onclick = () => {
+    mode = "color"
+};
+sizerB.onclick = () => changeSize();
+
+
+
 
 function createGrid(size) {
     const gridSquareSize = 512 / size;
@@ -9,6 +34,7 @@ function createGrid(size) {
         gridSquare.style.height = `${gridSquareSize}px`;
         gridSquare.style.width = `${gridSquareSize}px`;
 
+        gridSquare.addEventListener("mousedown", color);
         gridSquare.addEventListener("mouseover", color);
         container.appendChild(gridSquare);
     }
@@ -17,10 +43,23 @@ function createGrid(size) {
 
 
 function color(e) {
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    if (mode === "color" && e.type === "mouseover" && mouseDown) {
+        let r = Math.floor(Math.random() * 256);
+        let g = Math.floor(Math.random() * 256);
+        let b = Math.floor(Math.random() * 256);
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    } else if (mode === "eraser" && e.type === "mouseover" && mouseDown) {
+        let r = 255;
+        let g = 255;
+        let b = 255;
+        e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+    }
+}
+
+function changeSize() {
+    let size = parseInt(prompt("Input new size"));
+    container.innerHTML = "";
+    createGrid(size);
 }
 
 createGrid(16);
